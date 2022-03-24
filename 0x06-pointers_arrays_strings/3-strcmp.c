513f1db2a709c4fe9a6f69815be1655d32caa87f
Itenyo <kevinmmasi3@gmail.com>
2022-03-24 12:59:34 -0700

    my commit

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -1,26 +1,23 @@
-#include "holberton.h"
+#include "main.h"
+
 /**
  * _strcmp - compares two strings
- * @s1: string 1
- * @s2: string 2
- * Return: 0 if matching, and ns1 - ns2 if not matching
-*/
-
+ * @s1: first string to compare
+ * @s2: second string to compare
+ *
+ * Return: less than 0 if s1 is less than s2, 0 if they're equal,
+ * more than 0 if s1 is greater than s2
+ */
 int _strcmp(char *s1, char *s2)
 {
-int i;
-
-/**
- * We have reached the end of S1 OR we have reached the end of S2
- * 
-*/
-
-for (i = 0; s1[i] != '\0' || s2[i] != '\0'; i++)
-{
-	if (s1[i] != s2[i])
+	while (*s1 == *s2)
 	{
-		return (s1[i] - s2[i]);
+		if (*s1 == '\0')
+		{
+			return (0);
+		}
+		s1++;
+		s2++;
 	}
-}
-return (0);
+	return (*s1 - *s2);
 }
