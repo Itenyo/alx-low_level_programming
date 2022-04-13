81875c5cc20ee677e7bf2c8c103200ad15eae540
Itenyo <kevinmmasi3@gmail.com>
2022-04-13 12:10:39 -0700

    my commit

---
+++ 0x0F-function_pointers/3-main.c
@@ -0,0 +1,16 @@
+julien@ubuntu:~/0x0e. Function pointers$ gcc -Wall -pedantic -Werror -Wextra -std=gnu89 3-main.c 3-op_functions.c 3-get_op_func.c -o calc
+julien@ubuntu:~/0x0e. Function pointers$ ./calc 1 + 1
+2
+julien@ubuntu:~/0x0e. Function pointers$ ./calc 97 + 1
+98
+julien@ubuntu:~/0x0e. Function pointers$ ./calc 1024 / 10
+102
+julien@ubuntu:~/0x0e. Function pointers$ ./calc 1024 '*' 98
+100352
+julien@ubuntu:~/0x0e. Function pointers$ ./calc 1024 '\*' 98
+Error
+julien@ubuntu:~/0x0e. Function pointers$ ./calc 1024 - 98
+926
+julien@ubuntu:~/0x0e. Function pointers$ ./calc 1024 '%' 98
+44
+julien@ubuntu:~/0x0e. Function pointers$ 
