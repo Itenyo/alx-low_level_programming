e5229bb0407e89bfdbf2a21cfb9241f80586fedb
Itenyo <kevinmmasi3@gmail.com>
2022-04-13 12:40:27 -0700

    my commit

--- 0x0F-function_pointers/3-get_op_func.c
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -2,23 +2,23 @@
 #include <stdlib.h>
 
 /**
- * get_op_func - select the correct operation function asked by user
- * @s: operator argument
- * Return: function pointer corresponding to operator given
- */
+* get_op_func - select the correct operation function asked by user
+* @s: operator argument
+* Return: function pointer corresponding to operator given
+*/
 int (*get_op_func(char *s))(int, int)
 {
-	op_t ops[] = {
-		{"+", op_add},
-		{"-", op_sub},
-		{"*", op_mul},
-		{"/", op_div},
-		{"%", op_mod},
-		{NULL, NULL}
-	};
-	int i = 0;
+op_t ops[] = {
+{"+", op_add},
+{"-", op_sub},
+{"*", op_mul},
+{"/", op_div},
+{"%", op_mod},
+{NULL, NULL}
+};
+int i = 0;
 
-	while (ops[i].op != NULL && *(ops[i].op) != *s)
-		i++;
-	return (ops[i].f);
+while (ops[i].op != NULL && *(ops[i].op) != *s)
+i++;
+return (ops[i].f);
 }
--- 0x0F-function_pointers/_putchar.c
+++ 0x0F-function_pointers/_putchar.c
@@ -1,7 +1,13 @@
-#ifndef HOLBERTON_H
-#define HOLBERTON_H
-#include <stddef.h>
-void print_name(char *name, void (*f)(char *));
-void array_iterator(int *array, size_t size, void (*action)(int));
-int int_index(int *array, int size, int (*cmp)(int));
-#endif
+#include <unistd.h>
+
+/**
+* _putchar - writes the character c to stdout
+* @c: The character to print
+*
+* Return: On success 1.
+* On error, -1 is returned, and errno is set appropriately.
+*/
+int _putchar(char c)
+{
+return (write(1, &c, 1));
+}
