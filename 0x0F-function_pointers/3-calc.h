ad03937113619159675890c3c7fe012cc15f118a
Itenyo <kevinmmasi3@gmail.com>
2022-04-13 12:50:46 -0700

    my commit

--- 0x0F-function_pointers/3-calc.h
+++ 0x0F-function_pointers/3-calc.h
@@ -1,16 +1,18 @@
+#ifndef _CALC_H_
+#define _CALC_H_
 
-#ifndef CALC_H
-#define CALC_H
+#include <stdlib.h>
+#include <stdio.h>
 /**
- * struct op - Struct op
- *
- * @op: operator
- * @f: function
- */
+* struct op - Struct op
+*
+* @op: The operator
+* @f: The function associated
+*/
 typedef struct op
 {
-	char *op;
-	int (*f)(int a, int b);
+char *op;
+int (*f)(int a, int b);
 } op_t;
 
 int op_add(int a, int b);
@@ -19,4 +21,5 @@ int op_mul(int a, int b);
 int op_div(int a, int b);
 int op_mod(int a, int b);
 int (*get_op_func(char *s))(int, int);
+
 #endif
